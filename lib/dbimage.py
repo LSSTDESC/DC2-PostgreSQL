1020b1fea7753b2f338bab97e44920f052b49722
Joanne Bogart <jrb@slac.stanford.edu>
2019-06-11 18:19:18 -0700

    Add copyright notices for files written specifically for LSST/DESC

--- lib/dbimage.py
+++ lib/dbimage.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2016-2018  Sogo Mineo
+# Copyright (C) 2019  LSST Dark Energy Science Collaboration (DESC)
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,7 +16,7 @@
 # Adapted from dbtable.py
 from . import common
 from . import config
-#from .assumptions import Assumptions
+
 from .sourcetable import Field
 import numpy as np
 
