07f40ab9cd3994ce316265397949d7713b20491c
Joanne Bogart <jrb@slac.stanford.edu>
2019-06-25 11:35:07 -0700

    Create new native quantity forcedsourcevisit_good to avoid having to recompute in view Make use of forcedsourcevisit_good as well as new object catalog native quantity good when defining forced source view

--- lib/expressions.py
+++ lib/expressions.py
@@ -15,8 +15,20 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import re
+#from .sourcetable import Field
 
 #def __init__():
+'''
+  Static utilities to handle rpn expressions. One evaluates to 
+  another expression, to be evaluated later. The other 
+  evaluates immediately.   They recognize certain
+  special patterns: 
+       variables (xN, xMN, etc. where M, N,.. are digits  
+       local substitutions ({something},
+       array reference (starts with '@', refers to something in raw)
+       funcpat (describes function taking one variable
+       func2pat (describes function taking two variables) 
+'''
 varpat = re.compile('x(\d+)$')
 subspat = re.compile(r'\{[a-zA-Z_]*\}$')
 funcpat = re.compile('([a-zA-Z_]+[:a-zA-Z0-9_.]*)\(\)$')
@@ -146,6 +158,61 @@ def rpn_eval(inputs, rpn):
     if (len(argstack) != 1 ):
         raise ValueError('Bad RPN list')
     return argstack.pop()    
+
+def rpn_eval_array(inputs, rpn, rawfields):
+    '''
+    For this evaluation routine inputs are expected to refer to 
+    columns in the field list  fieldraw.  A new Field is returned
+
+    @param inputs     A list of column names in raw
+    @param rpn        List of tokens (operators and variables)
+    @param rawfields  Fields belonging to a SourceTable object
+
+    @return           data array which caller can use as data component of
+                      a new Field object
+    '''
+    #  Check that everything in inputs can be found in raw
+    for i in inputs:
+        if i not in rawfields:
+            raise KeyError('rp_eval_array: Unknown column ' + i)
+    #  Only choices for rpn tokens in this simpified implementation
+    #  are variables (x.. ) or operators.   To start recognize only
+    #  only or and not operators; add others later if needed.
+    #  First token must be a variable
+
+    argstack = []
+    for t in rpn:
+        tok = str(t)      #   should already be a string
+        m = varpat.match(tok)
+        if m:
+            i = int(m.group(1))
+            if i > len(inputs):
+                raise ValueErr('RPN elt {} references non-existent input'.format(tok))
+            argstack.append(rawfields[inputs[i-1]].data)
+            continue
+        # otherwise it had better be a known operator
+        if tok == 'or':
+            lft = argstack.pop()
+            rt = argstack.pop()
+            argstack.append(lft.__or__(rt))
+            continue
+        if tok == 'not':
+            v = argstack.pop()
+            argstack.append(v.__eq__(0))
+            continue
+        if tok == 'and':
+            lft = argstack.pop()
+            rt = argstack.pop()
+            argstack.append(lft.__and__(rt))
+            continue
+        # Should not get here
+        raise ValueErr('Unrecognized RPN token {}'.format(tok))
+
+    #  At this point there should be exactly one thing on the stack
+    if (len(argstack) != 1 ):
+        raise ValueError('Bad RPN list')
+    return argstack.pop()    
+    
         
 if __name__ =='__main__':
         
